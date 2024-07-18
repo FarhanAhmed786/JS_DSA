10214897f61a254524dfaa8ead28c13516f3cb3d
Farhan Ahmed <farhan2004ahmed@gmail.com>
2024-07-19 00:43:33 +0530

    Day 6.1

--- 30DaysJavaScript/day6.js
+++ 30DaysJavaScript/day6.js
@@ -30,3 +30,32 @@ console.log("UnShift value ",myArr[0]);
 // Task 7: Use the map method to create a new array where each number is doubled and log the new array.
 const mappedValue = myArr.map((a)=>(a*2))
 console.log(mappedValue);
+
+// Task 8: Use the filter method to create a new array with only even numbers and log the new array.
+const filterValue = myArr.filter((a)=>(a%2==0))
+console.log(filterValue);
+
+// Task 9: Use the reduce  method to calculate the sum of all numbers in the array and log the result.
+const sum = myArr.reduce((acc,cur)=>(acc+cur),0)
+console.log(sum);
+
+// Activity 4: Array Iterations
+
+// Task 10: Use a for loop to iterate over the array and log each element to the console
+for (let i = 0; i < myArr.length; i++) {
+    console.log(myArr[i]);    
+}
+
+// Task 11: Use a forEach method to iterate over the array and log each element to the console
+myArr.forEach(element => {
+    console.log(element);
+});
+
+// Activity 5: Multi-Dimensional Arrays
+
+// Task 12: Create a two-Dimensional array(matrix) and log the entire array to the console
+const twoDimArr = [[1,2,3],[4,5,6],[7,8,9]]
+console.table(twoDimArr);
+
+// Task 13: Access and log a specific element from the two dimensional array.
+console.log(twoDimArr[2][1]); 
